6097d5400a380720e8c38b5a2750c3820805b9ba
Teja P <teja2umesh@gmail.com>
2024-03-03 18:34:13 +0530

    Initial

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-export PREFIX="$HOME/opt/cross"
-export TARGET=i686-elf
-export PATH="$PREFIX/bin:$PATH"
+set -ex
 
-i686-elf-as boot.S -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+cargo build
 
+rm -fr isodir
 mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
+cp target/target/debug/kernel isodir/boot/myos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir
